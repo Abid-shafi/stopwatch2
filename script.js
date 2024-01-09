899b143c7471395ed677622b9295c8f05a60f087
Abid-shafi <144871167+Abid-shafi@users.noreply.github.com>
2024-01-09 13:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+var hr=0;
+var min=0;
+var s=0;
+var isStop=true;
+
+
+function start(){
+      if(isStop==true)
+      { 
+        isStop=false
+        timer();
+      }
+}
+
+function stop(){
+    isStop=true;
+}
+function reset(){
+    isStop = true;
+    s=0;
+    min=0;
+    hr=0;
+
+    thead.innerHTML="00 : 00 : 00";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function timer(){
+    if(isStop == false)
+    {
+       s=parseInt(s);
+       min=parseInt(min);
+       hr=parseInt(hr);
+       s++;
+
+       if(s==60){
+        s=0;
+        min++;
+       }
+       if(min==60){
+        m=0;
+        hr++;
+       }
+
+
+       if(s<10){
+        s="0" +s;
+       } 
+       if(min<10){
+        min="0" +min;
+       } 
+       if(hr<10){
+        hr="0" +hr;
+       } 
+            
+        thead.innerHTML=hr+" : "+min+" : "+s;
+        setTimeout("timer()",1000);
+
+    }
+}
